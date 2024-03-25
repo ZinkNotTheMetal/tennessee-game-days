eb6d5055010cbf875ce88bf718608578925f19db
William Zink <williamdzink@gmail.com>
2024-03-25 10:04:27 -0400

    feat(#42): Reporting all time top 20 games
    - Add API that returns top 20 games based on checkout minutes
    - Also returns BGG thing for the grid
    - Upgrade turbo
    
    refs: #40

---
+++ apps/admin/app/api/library/stats/all-time-top-20/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
+
+export const revalidate = 0; //Very important
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export async function GET() {
+  const top20CheckedOutGames = await prisma.libraryItem.findMany({
+    where: {
+      totalCheckedOutMinutes: {
+        gt: 0 // greater than 0
+      }
+    },
+    select: {
+      id: true,
+      alias: true,
+      barcode: true,
+      totalCheckedOutMinutes: true,
+      boardGameGeekId: true,
+      boardGameGeekThing: true,
+      _count: { select: { checkOutEvents: true }}
+    },
+    orderBy: {
+      totalCheckedOutMinutes: 'desc' // descending order
+    },
+    take: 20 // limit to the top 20 results
+  });
+
+  return NextResponse.json({
+    list: top20CheckedOutGames,
+  }, { status: 200 });
+}
