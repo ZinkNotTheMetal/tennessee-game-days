95d903d75e8da6e6840de38ef5f7e4a8869eeb05
William Zink <williamdzink@gmail.com>
2024-04-01 16:31:53 -0400

    fix ptw log add by figuring out convention id

--- apps/admin/app/api/play-to-win/log/route.ts
+++ apps/admin/app/api/play-to-win/log/route.ts
@@ -14,10 +14,29 @@ export async function POST(request: NextRequest) {
   if (data === undefined) return NextResponse.json({ error: 'Request is not correct, please verify your request'}, { status: 400 })
   if (data.attendeeIds === undefined || data.attendeeIds.length <= 0) return NextResponse.json({ error: 'No attendee Ids were linked and could not add the PTW play'}, { status: 400 })
 
+  const nextUpcomingConvention = await prisma.convention.findFirst({
+    where: {
+      endDateTimeUtc: {
+        gt: DateTime.utc().toISO()
+      }
+    },
+    include: {
+      venue: true
+    },
+    orderBy: {
+      startDateTimeUtc: 'asc'
+    }
+  })
+
+  if (nextUpcomingConvention === null) {
+    return NextResponse.json({ error: "Cannot log play to win as there are no conventions to log this play against" }, { status: 516 })
+  }
+
   const createManyData = data.attendeeIds.map(attendeeId => ({ attendeeId }))
 
   await prisma.playToWinPlay.create({
     data: {
+      conventionId: nextUpcomingConvention.id,
       checkedInTimeUtcIso: loggedTime,
       playToWinItemId: data.playToWinItemId,
       playToWinPlayAttendees: {
