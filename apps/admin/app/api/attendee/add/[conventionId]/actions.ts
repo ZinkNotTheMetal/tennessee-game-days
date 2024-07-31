92f78c29acda2e4dedec30ee4b40bf25cae31e6f
William Zink <williamdzink@gmail.com>
2024-07-30 21:40:20 -0400

    fixes related to readabilty

--- apps/admin/app/api/attendee/add/[conventionId]/actions.ts
+++ apps/admin/app/api/attendee/add/[conventionId]/actions.ts
@@ -1,26 +1,31 @@
+import {
+  IPurchasingPerson,
+  IPerson,
+  IEmergencyContact,
+} from "@/app/api/requests/add-attendee-request";
 import prisma from "@/app/lib/prisma";
 import { DateTime } from "ts-luxon";
 
 export async function GenerateBarcodeAndAddAttendee(
-  conventionId: number, 
-  personId: number, 
-  passPurchased: 'Free' | 'Individual' | 'Couple' | 'Family',
+  conventionId: number,
+  personId: number,
+  passPurchased: "Free" | "Individual" | "Couple" | "Family",
   stayingOnSite: boolean
-): Promise<{ success: boolean, barcode: string | null }> {
-  let success = false
-  let barcode: string | null = null
+): Promise<{ success: boolean; barcode: string | null }> {
+  let success = false;
+  let barcode: string | null = null;
   try {
-    const generatedBarcode = `${DateTime.now().toFormat('yy')}${conventionId}-${personId}`
+    const generatedBarcode = `${DateTime.now().toFormat("yy")}${conventionId}-${personId}`;
 
     // 4. Add a new barcode in a transaction
     prisma.$transaction(async (transaction) => {
       const newBarcode = await transaction.centralizedBarcode.create({
         data: {
           entityId: 0,
-          entityType: 'Attendee',
-          barcode: generatedBarcode
-        }
-      })
+          entityType: "Attendee",
+          barcode: generatedBarcode,
+        },
+      });
 
       const newAttendee = await transaction.attendee.create({
         data: {
@@ -29,25 +34,123 @@ export async function GenerateBarcodeAndAddAttendee(
           passPurchased: passPurchased,
           conventionId: conventionId,
           personId: personId,
-          dateRegistered: DateTime.utc().toISO()
-        }
-      })
+          dateRegistered: DateTime.utc().toISO(),
+        },
+      });
 
       await transaction.centralizedBarcode.update({
         where: { id: newBarcode.id },
-        data: { 
-          entityId: newAttendee.id
-        }
-      })
-
-    })
-    success = true
-    barcode = generatedBarcode
-
+        data: {
+          entityId: newAttendee.id,
+        },
+      });
+    });
+    success = true;
+    barcode = generatedBarcode;
   } catch (error) {
-    throw error
+    throw error;
   } finally {
-    return { success: success, barcode: barcode }
+    return { success: success, barcode: barcode };
+  }
+}
+
+export async function AddPurchasingPersonIntoSystem(
+  person: IPurchasingPerson,
+  emergencyContact: IEmergencyContact | undefined
+): Promise<number | undefined> {
+
+  let personId: number;
+
+  // Find first person with as much information as we can
+  const isPersonInSystem = await prisma.person.findFirst({
+    where: {
+      OR: [
+        { email: person.email ? person.email : undefined },
+        { phoneNumber: person.phoneNumber ? person.phoneNumber : undefined },
+        { 
+          firstName: person.preferredName || person.firstName,
+          lastName: person.lastName,
+        },
+        {
+          firstName: person.firstName,
+          preferredName: person.preferredName,
+          lastName: person.lastName,
+        },
+      ]
+    }
+  })
+
+  if (!isPersonInSystem) {
+    const personToAdd = await prisma.person.create({
+      data: {
+        // Merge existing data with provided data
+        firstName: person.firstName,
+        preferredName: person.preferredName,
+        lastName: person.lastName,
+        email: person.email,
+        phoneNumber: person.phoneNumber,
+        zipCode: person.zipCode,
+        emergencyContactName: emergencyContact?.name,
+        emergencyContactPhoneNumber: emergencyContact?.phoneNumber,
+        emergencyContactRelationship: emergencyContact?.relationship,
+      }
+    })
+    personId = personToAdd.id
+  } else {
+    const personToUpdate = await prisma.person.update({
+      where: { id: isPersonInSystem.id },
+      data: {
+        // Merge existing data with provided data
+        firstName: person.firstName || isPersonInSystem?.firstName,
+        preferredName: person.preferredName || isPersonInSystem?.preferredName,
+        lastName: person.lastName || isPersonInSystem?.lastName,
+        email: person.email || isPersonInSystem?.email,
+        phoneNumber: person.phoneNumber || isPersonInSystem?.phoneNumber,
+      }
+    })
+    personId = personToUpdate.id
   }
 
+  return personId;
+}
+
+
+export async function AddAdditionalPeopleUnderPurchasingPerson(personId: number, conventionId: number, additionalAttendees: IPerson[], passPurchased: "Free" | "Individual" | "Couple" | "Family", isStayingOnSite: boolean) 
+: Promise<{ personId: number, barcode: string | null }[]> 
+{
+  let barcodesCreated: { personId: number, barcode: string | null }[] = []
+
+  console.log(`Adding ${additionalAttendees.length} people under the person`)
+
+  for (const additionalAttendee of additionalAttendees) {
+    const additionalPersonToAdd = await prisma.person.upsert({
+      where: { 
+        firstName_lastName_relatedPersonId: {
+          firstName: additionalAttendee.firstName,
+          lastName: additionalAttendee.lastName,
+          relatedPersonId: personId
+        }
+       },
+      create: {
+        email: additionalAttendee.email || null,
+        firstName: additionalAttendee.firstName,
+        lastName: additionalAttendee.lastName,
+        preferredName: additionalAttendee.preferredName,
+        relatedPersonId: personId,
+      },
+      update: {
+        email: additionalAttendee.email || null,
+        firstName: additionalAttendee.firstName,
+        preferredName: additionalAttendee.preferredName,
+        lastName: additionalAttendee.lastName,
+      }
+    })
+
+    const response = await GenerateBarcodeAndAddAttendee(conventionId, additionalPersonToAdd.id, passPurchased, isStayingOnSite)
+    if (response.success) {
+      barcodesCreated.push({ personId: additionalPersonToAdd.id, barcode: response.barcode })
+    }
+  }
+
+  return barcodesCreated
 }
--- apps/admin/app/api/attendee/add/[conventionId]/route.ts
+++ apps/admin/app/api/attendee/add/[conventionId]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/app/lib/prisma";
-import { GenerateBarcodeAndAddAttendee } from './actions'
+import { AddAdditionalPeopleUnderPurchasingPerson, AddPurchasingPersonIntoSystem, GenerateBarcodeAndAddAttendee } from './actions'
 import { IAddAttendeeRequest } from "@/app/api/requests/add-attendee-request";
 import { AddAttendeeResponse } from "./response";
 
@@ -56,65 +56,18 @@ export async function POST(request: NextRequest, { params }: { params: { convent
   const json: IAddAttendeeRequest = await request.json();
   const { person, additionalPeople, isStayingOnSite, passPurchased } = json
   const { emergencyContact } = person
-  let personId: number = -1
   let barcodesCreated: { personId: number, barcode: string | null }[] = []
 
-  //1. Ensure convention id exists
+  // Ensure the convention exists that you are trying to add conventions to
   const conventionExists = await prisma.convention.count({
     where: { id: Number(params.conventionId)}
   })
 
   if (conventionExists <= 0) return NextResponse.json({ message: 'Unable to find convention that you are trying to add the Attendee for'}, { status: 400 })
 
-  // 2. Try to find person in the system the best we can...
-  const isPersonInSystem = await prisma.person.findFirst({
-    where: {
-      OR: [
-        { email: person.email ? person.email : undefined },
-        { phoneNumber: person.phoneNumber ? person.phoneNumber : undefined },
-        { 
-          firstName: person.preferredName || person.firstName,
-          lastName: person.lastName,
-        },
-        {
-          firstName: person.firstName,
-          preferredName: person.preferredName,
-          lastName: person.lastName,
-        },
-      ]
-    }
-  })
+  let personId = await AddPurchasingPersonIntoSystem(person, emergencyContact)
 
-  if (!isPersonInSystem) {
-    const personToAdd = await prisma.person.create({
-      data: {
-        // Merge existing data with provided data
-        firstName: person.firstName,
-        preferredName: person.preferredName,
-        lastName: person.lastName,
-        email: person.email,
-        phoneNumber: person.phoneNumber,
-        zipCode: person.zipCode,
-        emergencyContactName: emergencyContact?.name,
-        emergencyContactPhoneNumber: emergencyContact?.phoneNumber,
-        emergencyContactRelationship: emergencyContact?.relationship,
-      }
-    })
-    personId = personToAdd.id
-  } else {
-    const personToUpdate = await prisma.person.update({
-      where: { id: isPersonInSystem.id },
-      data: {
-        // Merge existing data with provided data
-        firstName: person.firstName || isPersonInSystem?.firstName,
-        preferredName: person.preferredName || isPersonInSystem?.preferredName,
-        lastName: person.lastName || isPersonInSystem?.lastName,
-        email: person.email || isPersonInSystem?.email,
-        phoneNumber: person.phoneNumber || isPersonInSystem?.phoneNumber,
-      }
-    })
-    personId = personToUpdate.id
-  }
+  if (personId === undefined) return NextResponse.json({ message: 'Unable to successfully add Purchasing Person into the system'}, { status: 523 })
 
   const attendeeAlreadyAdded = await prisma.attendee.findFirst({
     where: { personId: personId }
@@ -122,47 +75,22 @@ export async function POST(request: NextRequest, { params }: { params: { convent
 
   if (attendeeAlreadyAdded) return NextResponse.json({ error: 'Attendee has already been added to this convention'}, { status: 400 })
 
-  const response = await GenerateBarcodeAndAddAttendee(Number(params.conventionId), personId, passPurchased, isStayingOnSite)
-  if (response.success) {
-    barcodesCreated.push({ personId: personId, barcode: response.barcode})
-  } else {
+  console.log(`Adding Purchasing Person to Attendees - PersonID: ${personId} | ConventionID: ${params.conventionId}`)
+
+  let barcodeForPurchasingPerson = await GenerateBarcodeAndAddAttendee(Number(params.conventionId), personId, passPurchased, isStayingOnSite)
+  if (barcodeForPurchasingPerson.success) {
+    barcodesCreated.push({ personId: personId, barcode: barcodeForPurchasingPerson.barcode })
+  } else { 
     return NextResponse.json({
       message: 'Failed to add attendee as the barcode was already in the system',
     }, { status: 515 })
   }
 
+  console.log(`Purchasing Person has barcode created - ${barcodeForPurchasingPerson.barcode}`)
+
   if (additionalPeople !== undefined) {
-
-    for (const additionalAttendee of additionalPeople) {
-      const additionalPersonToAdd = await prisma.person.upsert({
-        where: { 
-          firstName_lastName_relatedPersonId: {
-            firstName: additionalAttendee.firstName,
-            lastName: additionalAttendee.lastName,
-            relatedPersonId: personId
-          }
-         },
-        create: {
-          email: additionalAttendee.email || null,
-          firstName: additionalAttendee.firstName,
-          lastName: additionalAttendee.lastName,
-          preferredName: additionalAttendee.preferredName,
-          relatedPersonId: personId,
-        },
-        update: {
-          email: additionalAttendee.email || null,
-          firstName: additionalAttendee.firstName,
-          preferredName: additionalAttendee.preferredName,
-          lastName: additionalAttendee.lastName,
-        }
-      })
-  
-      const response = await GenerateBarcodeAndAddAttendee(Number(params.conventionId), additionalPersonToAdd.id, passPurchased, isStayingOnSite)
-      if (response.success) {
-        barcodesCreated.push({ personId: additionalPersonToAdd.id, barcode: response.barcode })
-      }
-    }
-
+    await AddAdditionalPeopleUnderPurchasingPerson(personId, Number(params.conventionId), additionalPeople, passPurchased, isStayingOnSite)
+      .then(barcodes => barcodes.forEach(b => barcodesCreated.push(b)))
   }
 
   return NextResponse.json<AddAttendeeResponse>({
