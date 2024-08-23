c4b213e20a1d4a43b438853d07a28086c274261f
William Zink <williamdzink@gmail.com>
2024-08-22 23:26:15 -0500

    fix(updating): Fix barcode issue (#172)
    - Russ ran into an issue updating a barcode
    - This resolves it by putting the events in order
    - This solves the issue of updating and not reflecting in the table

--- apps/admin/app/api/library/edit/[id]/route.ts
+++ apps/admin/app/api/library/edit/[id]/route.ts
@@ -3,90 +3,109 @@ import prisma from "@/app/lib/prisma";
 import ILibraryItemRequest from "../../../requests/library-item-request";
 import { DateTime } from "ts-luxon";
 import { revalidateTag } from "next/cache";
+import { Prisma } from "@prisma/client";
 
 export const dynamic = "force-dynamic";
+
+
 
 // TODO: Add docs
 export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   const libraryItemToEdit: ILibraryItemRequest = await request.json();
-
   const { boardGameGeekThing, additionalBoxContent } = libraryItemToEdit;
   const { mechanics, id, ...bggRest } = boardGameGeekThing;
 
-  const upsertBggLibraryGame = await prisma.boardGameGeekThing.upsert({
-    where: { id: id },
-    update: bggRest,
-    create: {
-      ...bggRest,
-      id: id,
-    },
-  });
-
-  const updatedLibraryItem = await prisma.libraryItem.update({
-    where: { id: Number(params.id) },
-    data: {
-      alias:
-        libraryItemToEdit?.alias?.trim() === ""
-          ? null
-          : libraryItemToEdit.alias,
-      barcode: libraryItemToEdit.barcode,
-      isHidden: libraryItemToEdit.isHidden,
-      owner: libraryItemToEdit.owner,
-      boardGameGeekId: upsertBggLibraryGame.id,
-      updatedAtUtc: DateTime.utc().toISO(),
-    },
-  });
-
-  await prisma.centralizedBarcode.upsert({
+  const libraryItemToUpdate = await prisma.libraryItem.findFirstOrThrow({
     where: {
-      entityType_entityId: {
-        entityId: Number(updatedLibraryItem.id),
-        entityType: "LibraryItem",
-      },
-    },
-    create: {
-      entityId: Number(updatedLibraryItem.id),
-      entityType: "LibraryItem",
-      barcode: updatedLibraryItem.barcode,
-    },
-    update: {
-      barcode: updatedLibraryItem.barcode,
-    },
+      id: Number(params.id)
+    }
   })
 
-  for (const gm of mechanics) {
-    await prisma.mechanic.upsert({
-      where: { id: gm.id },
-      update: { name: gm.name },
-      create: {
-        id: gm.id,
-        name: gm.name,
-      },
-    });
+  await prisma.$transaction(async (
+    transaction: Prisma.TransactionClient
+  ) => {
 
-    await prisma.gameMechanic.upsert({
-      where: {
-        boardGameGeekId_mechanicId: {
-          boardGameGeekId: upsertBggLibraryGame.id,
-          mechanicId: gm.id,
-        },
-      },
+    // 1. Update the BGG thing in the database
+    const upsertBggLibraryGame = await transaction.boardGameGeekThing.upsert({
+      where: { id: id },
+      update: bggRest,
       create: {
-        mechanicId: gm.id,
+        ...bggRest,
+        id: id
+      }
+    })
+
+
+    if (libraryItemToUpdate?.barcode !== libraryItemToEdit.barcode) {
+      console.log('change of barcode')
+
+      await transaction.centralizedBarcode.update({
+        where: {
+          entityType_entityId: {
+            entityType: 'LibraryItem',
+            entityId: Number(params.id)
+          }
+        },
+        data: {
+          barcode: libraryItemToEdit.barcode
+        }
+      })
+
+    }
+
+    // 2. Check to see if the barcode is the same
+    await transaction.libraryItem.update({
+      where: { id: Number(params.id) },
+      data: {
+        alias:
+          libraryItemToEdit?.alias?.trim() === ""
+            ? null
+            : libraryItemToEdit.alias,
+        barcode: libraryItemToEdit.barcode,
+        isHidden: libraryItemToEdit.isHidden,
+        owner: libraryItemToEdit.owner,
         boardGameGeekId: upsertBggLibraryGame.id,
+        updatedAtUtc: DateTime.utc().toISO(),
       },
-      update: {
-        mechanicId: gm.id,
-        boardGameGeekId: upsertBggLibraryGame.id,
-      },
-    });
-  }
+    })
+
+    for (const gm of mechanics) {
+      await transaction.mechanic.upsert({
+        where: { id: gm.id },
+        update: { name: gm.name },
+        create: {
+          id: gm.id,
+          name: gm.name,
+        },
+      })
+
+      await transaction.gameMechanic.upsert({
+        where: {
+          boardGameGeekId_mechanicId: {
+            boardGameGeekId: upsertBggLibraryGame.id,
+            mechanicId: gm.id,
+          },
+        },
+        create: {
+          mechanicId: gm.id,
+          boardGameGeekId: upsertBggLibraryGame.id,
+        },
+        update: {
+          mechanicId: gm.id,
+          boardGameGeekId: upsertBggLibraryGame.id,
+        },
+      });
+    }
+
+  })
 
   revalidateTag('library')
 
   return NextResponse.json(
     {
-      message: `Successfully edited ${upsertBggLibraryGame.itemName} in library`,
+      id: libraryItemToUpdate.id,
+      barcode: libraryItemToEdit.barcode,
+      message: `Successfully edited - ${libraryItemToUpdate.id} - ${libraryItemToUpdate.alias ?? boardGameGeekThing.itemName} in library`,
     },
     { status: 200 }
   );
